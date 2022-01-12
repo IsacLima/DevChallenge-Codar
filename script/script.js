d6c5e868093ec0620273b1c2f73ce84fd65424e7
Isaque Lima Leite <30935061+IsacLima@users.noreply.github.com>
2022-01-12 01:58:43 -0300

    Update script.js
    Adicionado o sroll suave até as seções.

--- script/script.js
+++ script/script.js
@@ -1,1 +1,22 @@
+const menuItens = document.querySelectorAll('.menu a[href^="#"]');
 
+function scrollParaSecao(to){
+	window.scroll({
+		top: to,
+		behavior: 'smooth'
+	})
+}
+
+function topoDaSecao(event){
+	event.preventDefault();
+	console.log(event.currentTarget.hash.offsetTop);
+	var topo = document.getElementById(event.currentTarget.hash).offsetTop;
+	if(event.currentTarget.hash != "#rodape"){
+		topo = topo - 100;
+	}
+	scrollParaSecao(topo);
+}
+
+menuItens.forEach(item =>{
+	item.addEventListener('click', topoDaSecao)
+})
